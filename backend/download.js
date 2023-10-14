09d42b0f5e6745fb4249223382d90adbd3cc9ef6
Marvin Osolo <mansolo.osolo51@gmail.com>
2023-10-14 12:47:39 +0300

    You can download back files with extensions

---
+++ backend/download.js
@@ -0,0 +1,27 @@
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const { BUCKET_NAME } = process.env;
+const EXPIRY_DEFAULT = 60
+const s3Client = new S3Client(); 
+
+export const handleEvent = async (event, context) => {
+    //extract the id from the URL (path parameter = id)
+    const id = event.pathParameters.id;
+    // create a presigned URL for the file in the bucket with the given id
+    const key  = `shares/${id[0]}/${id[1]}/${id}`;
+    const getCommand = new GetObjectCommand({
+        Bucket: BUCKET_NAME,
+        Key: key,
+    });
+    const downloadUrl = await getSignedUrl(s3Client, getCommand, {
+        expiresIn: EXPIRY_DEFAULT,
+    });
+    // return the HTTP redirect response to the presigned URL
+    return {
+        statusCode: 301,
+        headers: {
+            Location: downloadUrl,
+        },
+    };
+}
--- backend/share-handler.js
+++ backend/share-handler.js
@@ -1,44 +1,51 @@
-import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import {randomUUID} from 'node:crypto';
 
 
-const { BUCKET_NAME } = process.env;
+const { BUCKET_NAME, BASE_URL } = process.env;
 const EXPIRY_DEFAULT = 60 * 60 * 24; // 24 hours
 const s3Client = new S3Client();
+const MIME_TYPE = 'application/octet-stream';
 
 export const handleEvent = async (event, context) => {
   const id = randomUUID();
   const key = `shares/${id[0]}/${id[1]}/${id}`;
   // Create a key (filename)
 
+  const filename = event?.queryStringParameters?.filename;
+  const contentDisposition = filename && `attachment; filename="${filename}"`;
+  const contentDispositionHeader = contentDisposition && `content-disposition: ${contentDisposition}`;
+
   
   // Create and Return the Download URL
-  const getCommand = new GetObjectCommand({
-    Bucket: BUCKET_NAME,
-    Key: key,
-  });
-
-  const retrievalUrl = await getSignedUrl(s3Client, getCommand, {
-    expiresIn: EXPIRY_DEFAULT,
-  });
+ const downloadUrl = `${BASE_URL}/share/${id}`;
 
   // Create an Upload URL
   const putCommand = new PutObjectCommand({
     Bucket: BUCKET_NAME,
     Key: key,
+    ContentDisposition: contentDisposition,
   });
+
+  const signableHeaders = new Set([`content-type: ${MIME_TYPE}`])
+  if (contentDisposition) {
+    signableHeaders.add(contentDispositionHeader)
+  }
 
   const uploadUrl = await getSignedUrl(s3Client, putCommand, {
     expiresIn: EXPIRY_DEFAULT,
+    signableHeaders,
   });
 
   return {
     statusCode: 201,
     body: `
-    Upload with: curl -X PUT -T <filename> '${uploadUrl}'
+    Upload with: curl -X PUT -T ${filename || '<FILENAME>'} ${contentDispositionHeader? `-H '${contentDispositionHeader}'` : ''} '${uploadUrl}'
 
-    Download with: curl ${retrievalUrl}`,
+    Download with: curl ${downloadUrl}
+    
+    `,
   };
   
 }
